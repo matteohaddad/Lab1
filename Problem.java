84f60bc072db5cc76fbb149932af68c5bb41b95f
Matteo Haddad <matteohaddad04@gmail.com>
2023-01-24 21:24:06 +0200

    final

---
+++ Problem.java
@@ -0,0 +1,7 @@
+public class Problem{
+	public static void Main (String[]args){
+		System.out.println("!!!!!!!");
+		System.out.println("Hello, there are a lot");
+		System.out.print("of problems");
+	}
+}
