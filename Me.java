b43aab28aeaf89f2019f74ec7996c0bea8572e94
Matteo Haddad <matteohaddad04@gmail.com>
2023-01-25 12:30:10 +0200

    My name, major and age

---
+++ Me.java
@@ -0,0 +1,10 @@
+public class Me{
+	public static void main (String[] args) {
+		System.out.println("Full name : Matteo Haddad");
+		System.out.println("Age : 18");
+		System.out.println("Major : computer engineering");
+
+
+
+	}
+}
